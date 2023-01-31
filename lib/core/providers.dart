0a4bcd5591a911ca95cd11c2f4d58cffcb488d44
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-01-31 22:45:53 +0500

    Authentication

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -4,6 +4,11 @@ import 'package:appwrite/appwrite.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/utils/constants/constants.dart';
 
-final appwriteClientProvider = Provider((ref) {
+final appwriteClientProvider = Provider<Client>((ref) {
   Client client = Client();
+  return client.setEndpoint(AppwriteConstants.endPoint)
+  .setProject(AppwriteConstants.projectId)
+  .setSelfSigned(status: true);
 });
+
+
