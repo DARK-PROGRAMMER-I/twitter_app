7a0752a11db93903cd6dc1fb47a54dda17b281ea
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-01 12:34:15 +0500

    Authentication

---
+++ lib/features/auth/controllers/auth_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/apis/auth/auth_api.dart';
+
+import '../../../utils/utils.dart';
+
+final authControllerProvider = Provider((ref) => null)
+
+class AuthController extends StateNotifier<bool>{
+  AuthApi _authApi;
+  AuthController({required AuthApi authApi}) : _authApi = authApi, super(false);
+
+  Future<void> signup({
+  required String email,
+  required String password,
+  required BuildContext context,
+})async{
+    state = true;
+    final result = await _authApi.signup(email: email, password: password);
+    result.fold(
+            (l){
+              showSnakBacr(context, l.message);
+              state = false;
+            },
+            (r){
+              debugPrint(r.email);
+            });
+  }
+}
