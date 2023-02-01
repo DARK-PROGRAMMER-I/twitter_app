a423f8573209c9d5ce367cd3b6d5f59ffe2b48f1
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-01 13:11:17 +0500

    Authentication

--- lib/utils/constants/appwrite_constants.dart
+++ lib/utils/constants/appwrite_constants.dart
@@ -1,5 +1,5 @@
 class AppwriteConstants {
   static const String databaseId = "63d76c7658e005270d87";
   static const String projectId = "63d769b68ed50129f51b";
-  static const String endPoint = "http://127.0.0.1:80/v1";
+  static const String endPoint = "http://192.168.10.10/v1";
 }
