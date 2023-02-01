eb05c561913c3a70f7df787b3af37e3b36c6ba7e
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-01 23:02:53 +0500

    home

---
+++ lib/utils/common/error_view.dart
@@ -0,0 +1,26 @@
+import 'exports.dart';
+
+class ErrorWidget extends StatelessWidget {
+  final String message;
+  const ErrorWidget({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(message),
+    );
+  }
+}
+
+class ErrorScreen extends StatelessWidget {
+  final String message;
+  const ErrorScreen({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ErrorWidget(message: message,),
+    );
+  }
+}
+
