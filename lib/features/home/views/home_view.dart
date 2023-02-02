90b7c7f59e30cd6753f86a0b1a7f7ea4533ab3c0
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-02 22:07:18 +0500

    home

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -3,39 +3,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/features/home/controllers/navigation_controller.dart';
 import 'package:twitter_clone/utils/constants/constants.dart';
-import 'package:twitter_clone/utils/theme/pallete.dart';
 
 import '../../../utils/common/exports.dart';
 
-final navigationProvider = StateNotifierProvider<NavigationController, int>((ref) =>NavigationController() );
-
-class HomeView extends ConsumerWidget {
+class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: UIConstants.appBar(),
       bottomNavigationBar: CupertinoTabBar(
         onTap: (int value){
-          ref.read(navigationProvider.notifier).updateState(value);
         },
-        currentIndex: ref.watch(navigationProvider.notifier).state,
         items: [
           BottomNavigationBarItem(
-              icon: ref.watch(navigationProvider.notifier).state == 0?
-              SvgPicture.asset(AssetsConstants.homeFilledIcon, color: Pallete.whiteColor,):
-              SvgPicture.asset(AssetsConstants.homeOutlinedIcon, color: Pallete.whiteColor,)
+              icon: SvgPicture.asset(AssetsConstants.homeFilledIcon)
           ),
           BottomNavigationBarItem(
-              icon: ref.watch(navigationProvider.notifier).state == 1?
-              SvgPicture.asset(AssetsConstants.searchIcon, color: Pallete.whiteColor,):
-              SvgPicture.asset(AssetsConstants.searchIcon, color: Pallete.whiteColor,)
+              icon: SvgPicture.asset(AssetsConstants.homeFilledIcon)
           ),
           BottomNavigationBarItem(
-              icon: ref.watch(navigationProvider.notifier).state == 2?
-              SvgPicture.asset(AssetsConstants.notifFilledIcon, color: Pallete.whiteColor,):
-              SvgPicture.asset(AssetsConstants.notifOutlinedIcon, color: Pallete.whiteColor,)
+              icon: SvgPicture.asset(AssetsConstants.homeFilledIcon)
           )
         ],
       ),
