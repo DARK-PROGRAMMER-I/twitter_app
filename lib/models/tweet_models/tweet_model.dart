b194e207106d56044f8b08feca7fd2caf4e486d8
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-04 14:57:30 +0500

    tweet

--- lib/models/tweet_models/tweet_model.dart
+++ lib/models/tweet_models/tweet_model.dart
@@ -2,8 +2,8 @@ class TweetModel{
   final String text;
   final List<String> hashTags;
   final List<String> links;
-  final List<String> text;
-  final String text;
+  final List<String> imageLinks;
+  final String uid;
   final String text;
   final String text;
   final String text;
