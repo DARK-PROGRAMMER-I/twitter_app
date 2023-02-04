f0347a3437e1486640eeaf580ce61ca012de4916
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-04 14:56:08 +0500

    tweet

---
+++ lib/features/tweet/controllers/tweet_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TweetController extends StateNotifier<bool>{
+  TweetController() : super(false);
+
+  
+}
---
+++ lib/models/tweet_models/tweet_model.dart
@@ -0,0 +1,12 @@
+class TweetModel{
+  final String text;
+  final List<String> hashTags;
+  final String text;
+  final String text;
+  final String text;
+  final String text;
+  final String text;
+  final String text;
+  final String text;
+
+}
