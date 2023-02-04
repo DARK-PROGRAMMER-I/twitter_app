cf9046687eb1776f125c59dd499581723ea4e8ed
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-04 09:41:02 +0500

    tweet

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 showSnakBacr(BuildContext context, String content){
   ScaffoldMessenger.of(context).showSnackBar(
@@ -11,4 +14,14 @@ showSnakBacr(BuildContext context, String content){
 
 String getNameFromEmail(String email){
   return email.split('@')[0];
+}
+
+Future<List<File>> pickMultipleImages()async{
+  List<File> images = [];
+  ImagePicker picker = ImagePicker();
+  List<XFile> files = await picker.pickMultiImage();
+  files.forEach((element) {
+    images.add(File(element.path));
+  });
+  return images;
 }
