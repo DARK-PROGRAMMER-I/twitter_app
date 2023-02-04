84be263ff01f3addc7d11b94271480a20b93a4ba
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-04 09:30:14 +0500

    tweet

---
+++ lib/features/tweet/widgets/carousal_images.dart
@@ -0,0 +1,10 @@
+import '../../../utils/common/exports.dart';
+
+class CarousalImages extends StatelessWidget {
+  const CarousalImages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
