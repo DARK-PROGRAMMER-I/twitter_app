a2b851d51c97c864cb44c4dadeadccbf5b0aaf0a
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-04 09:33:01 +0500

    tweet

--- lib/features/tweet/widgets/carousal_images.dart
+++ lib/features/tweet/widgets/carousal_images.dart
@@ -1,10 +1,23 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
+
 import '../../../utils/common/exports.dart';
 
 class CarousalImages extends StatelessWidget {
-  const CarousalImages({Key? key}) : super(key: key);
+  final List<File> images;
+  const CarousalImages({Key? key, required this.images}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CarouselSlider(
+        items: images.map((file) {
+          return Image.file(file);
+        }).toList(),
+        options: CarouselOptions(
+          animateToClosest: false,
+          autoPlay: false,
+          scrollDirection: Axis.horizontal
+        ));
   }
 }
