4bd73c24b27f9403c6940818458d018270386a1e
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-05 12:09:56 +0500

    tweet

--- lib/features/tweet/controllers/tweet_controller.dart
+++ lib/features/tweet/controllers/tweet_controller.dart
@@ -2,7 +2,12 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/core/enums/tweet_type.dart';
+import 'package:twitter_clone/features/auth/controllers/auth_controller.dart';
+import 'package:twitter_clone/models/tweet_models/tweet_model.dart';
 import 'package:twitter_clone/utils/utils.dart';
+
+import '../../../models/user_models/user_model.dart';
 
 class TweetController extends StateNotifier<bool>{
   final Ref _ref ;
@@ -50,7 +55,20 @@ class TweetController extends StateNotifier<bool>{
   }){
     final List<String> hashtags = _extractHashtTags(text: tweetText);
     final String link = _extractLinks(text: tweetText);
-    final String uid = _ref.read();
+    final UserModel? user = _ref.read(currentUserDetailsProvider).value;
+    TweetModel tweetModel = TweetModel(
+        text: tweetText,
+        hashTags: hashtags,
+        link: link,
+        imageLinks: [],
+        uid: user?.uid ?? '',
+        tweetType: TweetType.text,
+        tweetAt: DateTime.now(),
+        likes: [],
+        commentIds: [],
+        tweetId: '',
+        reshareCount: reshareCount
+    );
   }
 
   // Extract HashTags
