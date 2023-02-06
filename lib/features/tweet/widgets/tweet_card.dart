bad58401d40301ec39d326d1a2d150bffa7d836b
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-06 14:57:21 +0500

    tweet

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -2,9 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/features/auth/controllers/auth_controller.dart';
 import 'package:twitter_clone/models/tweet_models/tweet_model.dart';
 
-import '../../../utils/common/error_view.dart';
 import '../../../utils/common/exports.dart';
-import '../../../utils/common/loading_indicator.dart';
 
 class TweetCardWidget extends ConsumerWidget {
   final TweetModel tweet;
@@ -15,7 +13,11 @@ class TweetCardWidget extends ConsumerWidget {
     return ref.watch(userDetailsProvider(tweet.uid)).
     when(
         data: (user){
+          return Column(
+            children: [
 
+            ],
+          );
         },
         error: (error, stackTrace) {
           print(stackTrace);
--- lib/utils/common/exports.dart
+++ lib/utils/common/exports.dart
@@ -1,3 +1,5 @@
 export 'rounded_small_button.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter/material.dart';
+export 'loading_indicator.dart';
+export 'error_view.dart';
