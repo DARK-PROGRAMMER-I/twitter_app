83da093498431444b92af9602e5b3d417330d119
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-06 14:56:00 +0500

    tweet

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -1,14 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/features/auth/controllers/auth_controller.dart';
 import 'package:twitter_clone/models/tweet_models/tweet_model.dart';
 
+import '../../../utils/common/error_view.dart';
 import '../../../utils/common/exports.dart';
+import '../../../utils/common/loading_indicator.dart';
 
-class TweetCardWidget extends StatelessWidget {
+class TweetCardWidget extends ConsumerWidget {
   final TweetModel tweet;
   const TweetCardWidget({Key? key, required this.tweet}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(userDetailsProvider(tweet.uid)).
+    when(
+        data: (user){
+
+        },
+        error: (error, stackTrace) {
+          print(stackTrace);
+          return ErrorrWidget(message: error.toString());
+        },
+        loading: ()=> const LoadingWidget()
 
     );
   }
