bae86be16703dee26efea01244298299accbb777
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-06 14:59:34 +0500

    tweet

---
+++ lib/features/tweet/widgets/hash_tagged_text.dart
@@ -0,0 +1,10 @@
+import '../../../utils/common/exports.dart';
+
+class HashTaggedText extends StatelessWidget {
+  const HashTaggedText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
