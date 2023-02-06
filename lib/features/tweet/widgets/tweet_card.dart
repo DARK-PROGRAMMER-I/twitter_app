617e9b8a8f7df277fcfb1f9db58e40029d26ac7d
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-06 14:51:45 +0500

    tweet

---
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -0,0 +1,15 @@
+import 'package:twitter_clone/models/tweet_models/tweet_model.dart';
+
+import '../../../utils/common/exports.dart';
+
+class TweetCardWidget extends StatelessWidget {
+  final TweetModel tweet;
+  const TweetCardWidget({Key? key, required this.tweet}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+}
--- lib/features/tweet/widgets/tweet_list.dart
+++ lib/features/tweet/widgets/tweet_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/features/tweet/controllers/tweet_controller.dart';
+import 'package:twitter_clone/features/tweet/widgets/tweet_card.dart';
 import 'package:twitter_clone/models/tweet_models/tweet_model.dart';
 import 'package:twitter_clone/utils/common/error_view.dart';
 import 'package:twitter_clone/utils/common/loading_indicator.dart';
@@ -18,9 +19,7 @@ class TweetList extends ConsumerWidget {
             itemCount: tweetList.length,
             itemBuilder: (BuildContext context, int index){
                 TweetModel tweet = tweetList[index];
-                return ListTile(
-                  title: Text(tweet.text),
-                );
+                return TweetCardWidget(tweet: tweet);
               }
           );
         },
