684c32c6052a288e636328968baa37fe18a97c29
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-06 15:38:57 +0500

    tweet

--- lib/features/tweet/widgets/carusal_slider_widget.dart
+++ lib/features/tweet/widgets/carusal_slider_widget.dart
@@ -1,11 +1,19 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:twitter_clone/utils/theme/pallete.dart';
 
 import '../../../utils/common/exports.dart';
 
-class CarusalSliderWidget extends StatelessWidget {
+class CarusalSliderWidget extends StatefulWidget {
   final List<dynamic> images;
   const CarusalSliderWidget({Key? key, required this.images}) : super(key: key);
+
+  @override
+  State<CarusalSliderWidget> createState() => _CarusalSliderWidgetState();
+}
+
+class _CarusalSliderWidgetState extends State<CarusalSliderWidget> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +29,7 @@ class CarusalSliderWidget extends StatelessWidget {
       child: Column(
         children: [
           CarouselSlider(
-              items: images.map((image) {
+              items: widget.images.map((image) {
                 return Image.network(image);
               }).toList(),
               options: CarouselOptions(
@@ -31,7 +39,11 @@ class CarusalSliderWidget extends StatelessWidget {
                   scrollDirection: Axis.horizontal,
                   enableInfiniteScroll: false
               )),
-
+          Container(
+            child: Row(
+              children: List.generate(widget.images.length, (index) => null),
+            ),
+          )
         ],
       ),
     );
