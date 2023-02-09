cd0d891e37241cba0ea091119b4e626e611b8dfc
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-02-09 15:51:39 +0500

    tweet

--- lib/utils/constants/appwrite_constants.dart
+++ lib/utils/constants/appwrite_constants.dart
@@ -1,3 +1,5 @@
+
+
 class AppwriteConstants {
   static const String databaseId = "63d76c7658e005270d87";
   static const String projectId = "63d769b68ed50129f51b";
@@ -10,5 +12,5 @@ class AppwriteConstants {
 
   static const String imageBucketId = "63dfa0eb5f81ccf78df4";
   static String generateImageUrl(String id)=>
-  '$endPoint/storage/buckets/$imageBucketId/files/$id/view?project=$projectId&mode=admin';
+      '$endPoint/storage/buckets/$imageBucketId/files/$id/view?project=$projectId&mode=admin';
 }
